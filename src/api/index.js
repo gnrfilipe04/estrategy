1b520ec32baf8ea8203573951d019ac72b43d599
gnrfilipe04 <gnr_filipe@hotmail.com>
2023-01-17 00:57:27 -0300

    :sparkles: add call of dependencies

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImludF9hcG9udGFtZW50b3MiLCJuYmYiOjE2NzM4MzY0MzgsImV4cCI6MTY3Mzg0MDAzOCwiaWF0IjoxNjczODM2NDM4fQ.JWw88ScvyGSP8WNpOF45UbniY-csYicDCHnZU8JrkQU'
+const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImludF9hcG9udGFtZW50b3MiLCJuYmYiOjE2NzM5MjQyNjYsImV4cCI6MTY3MzkyNzg2NiwiaWF0IjoxNjczOTI0MjY2fQ.cU5l4Guap3Kz8VJmTm7CcDhUO-rjFEJoWL-m4j1lIwU'
 
 const api = axios.create({
     baseURL: 'https://barramentoatvos-dev.azurewebsites.net/api/',
--- src/strategies/plantioStrategy.js
+++ src/strategies/plantioStrategy.js
@@ -2,39 +2,66 @@ import { api } from "../api/index.js"
 
 export default class PlantioStrategy {
 
-    async _takeOrder(){
+    async _takeOrders(){
         const {data} = await api.get('/plant/filtro?fgControle=I&app_dt_lastupdate=2021-12-31')
 
-        return data
+        return { PLNOrders: data }
     }
 
     async _takeSpacing(){
-        const {data} = await api.get('/plant/filtro?fgControle=I&app_dt_lastupdate=2021-12-31')
+        const {data} = await api.get('/espacam/filtro?instancias=31&fgControle=I&app_dt_lastupdate=2021-12-31')
 
-        return data
+        return { PLNSpacing: data } 
     }
 
     async _takeFronts(){
-        
+        const {data} = await api.get('/frentPlant/filtro?fgControle=I&app_dt_lastupdate=2021-12-31')
+
+        return { PLNFronts: data } 
     }
 
     async _takeSystems(){
-        
+        const {data} = await api.get('/sistplans/filtro?fgControle=I&app_dt_lastupdate=2021-12-31')
+
+        return { PLNSystems: data }
     }
 
     async _takeFarms(){
+        const { data } = await api.get('/histprepro/filtro?fgControle=I&app_dt_lastupdate=2021-12-31')
 
+        return { PLNFarms: data }
     }
 
     async _takeVarieties(){
+        const { data } = await api.get('/variedade/filtro?fgControle=I&app_dt_lastupdate=2021-12-31')
 
+        return { PLNVarieties: data } 
     }
 
     async _takeDependencies(){
+        const promisses =  [
+            this._takeOrders(),
+            this._takeSystems(),
+            this._takeSpacing(),
+            this._takeFronts(),
+            this._takeVarieties(),
+            this._takeFarms()
+        ]
 
+        const result = await Promise.allSettled(promisses)
+
+        const containsRejected = result.find(call => call.status === 'rejected')
+
+        if(containsRejected) return containsRejected
+
+        const values = result.reduce((acc, call) => ({...acc, ...call.value}), {})
+        
+        return values
     }
 
     async sycronization(){
+        const dependencies = await this._takeDependencies()
+        console.log(Object.keys(dependencies))
         console.log('Plantio Sync')
     }
 }
